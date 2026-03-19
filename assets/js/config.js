75cb177c92e1a8b1237f0f197b5f8ee977711289
generalberlin <btsreklam123@gmail.com>
2026-03-19 13:35:28 +0300

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+window.SITE_CONFIG = {
+  companyName: 'GARENT KURUMSAL FİLO',
+  phoneDisplay: '0501 687 42 34',
+  phoneRaw: '905016874234',
+  whatsappRaw: '905016874234',
+  whatsappMessage: 'Merhaba, araç rezervasyonu yapmak istiyorum.',
+  locations: ['İstanbul Havalimanı', 'Sabiha Gökçen', 'Taksim', 'Kadıköy', 'Beşiktaş', 'Bursa Merkez'],
+  workingHours: '7/24 teslimat ve rezervasyon desteği'
+};
